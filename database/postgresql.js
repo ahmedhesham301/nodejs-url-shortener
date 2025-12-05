f7b5a195533bced46fe1d167b6a5efdc7c5f77f9
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2025-12-06 00:18:55 +0200

    added plan and moniotring type to the sql

--- database/postgresql.js
+++ database/postgresql.js
@@ -4,16 +4,21 @@ export const pool = new Pool()
 
 export async function initDB() {
     const query = `
+    CREATE TYPE monitoring_type AS ENUM ('none', 'basic', 'detailed');
+    CREATE TYPE plan_type AS ENUM ('free', 'basic', 'pro');
     CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
         email VARCHAR(255) NOT NULL UNIQUE,
         password_hash VARCHAR(255) NOT NULL,
         created_at TIMESTAMP DEFAULT now()
+        plan plan_type NOT NULL,
+        plan_expiration DATE NOT NULL,
     );
     CREATE TABLE IF NOT EXISTS urls (
 	  id VARCHAR PRIMARY KEY,
       user_id INTEGER NOT NULL REFERENCES users(id),
-	  url VARCHAR NOT NULL
+	  url VARCHAR NOT NULL,
+      monitoring monitoring_type NOT NULL
 	);
     `
     try {
