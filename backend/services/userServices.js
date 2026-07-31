bbafb6511f8a7e418cf80727be3eda404e8a1f6c
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2026-07-31 17:24:32 +0300

    test commit

--- backend/services/userServices.js
+++ backend/services/userServices.js
@@ -1,32 +1,32 @@
-import bcrypt from "bcrypt"
-import { save, findByEmail } from "../models/userModel.js";
-import { trace } from "@opentelemetry/api";
+    import bcrypt from "bcrypt"
+    import { save, findByEmail } from "../models/userModel.js";
+    import { trace } from "@opentelemetry/api";
 
-const tracer = trace.getTracer();
+    const tracer = trace.getTracer("backend");
 
-export async function registerUser(email,password) {
-    const span = tracer.startSpan("registerUser");
+    export async function registerUser(email,password) {
+        const span = tracer.startSpan("registerUser");
 
-    const passwordHash = await bcrypt.hash(password, 10);
-    await save(email, passwordHash)
-    
-    span.end()
-}
-
-export async function authenticateUser(email, password) {
-    const userData = await findByEmail(email)
-
-    if (userData == null) {
-        let err = new Error("Email not found");
-        err.code = 'EMAIL_NOT_FOUND'
-        throw  err
-    }
-    
-    if (await bcrypt.compare(password, userData.passwordHash)) {
-        return userData
+        const passwordHash = await bcrypt.hash(password, 10);
+        await save(email, passwordHash)
+        
+        span.end()
     }
 
-    let err = new Error("wrong password");
-    err.code = 'WRONG_PASSWORD'
-    throw err
-}
+    export async function authenticateUser(email, password) {
+        const userData = await findByEmail(email)
+
+        if (userData == null) {
+            let err = new Error("Email not found");
+            err.code = 'EMAIL_NOT_FOUND'
+            throw  err
+        }
+        
+        if (await bcrypt.compare(password, userData.passwordHash)) {
+            return userData
+        }
+
+        let err = new Error("wrong password");
+        err.code = 'WRONG_PASSWORD'
+        throw err
+    }
