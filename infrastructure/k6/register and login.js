4a19e9a6a0bc0e9a2217de055f0fbde60e65684c
Ahmed Hesham <ahmed.hesham.farag@gmail.com>
2026-08-02 18:21:46 +0300

    feat: update k6 script to use dynamic host for API endpoints; increase virtual users for load testing

--- infrastructure/k6/register and login.js
+++ infrastructure/k6/register and login.js
@@ -4,11 +4,13 @@ import exec from "k6/execution";
 
 
 export const options = {
-  vus: 1,
-  duration: '30s'
+  vus: 10,
+  duration: '30s',
+  // Iterations: 1,
+  noConnectionReuse: true,
 };
 
-
+const host = __ENV.HOSTNAME;
 export default function () {
   const payload = JSON.stringify({
     email: randomEmail(),
@@ -23,7 +25,7 @@ export default function () {
 
   // register 
   group("register", function () {
-    const registerRes = http.post('http://10.43.254.101:8080/auth/register', payload, params)
+    const registerRes = http.post(`${host}/auth/register`, payload, params)
     check(registerRes, { 'response code for register was 201': (res) => res.status == 201 })
     if (registerRes.status !== 201) {
       console.log(registerRes.body);
@@ -32,30 +34,30 @@ export default function () {
 
   })
 
-  sleep(1)
+  // sleep(1)
 
   //login
-  group("login", function () {
-    const loginRes = http.post('http://10.43.254.101:8080/auth/login', payload, params)
-    check(loginRes, { 'response code for login was 200': (res) => res.status == 200 })
-    if (loginRes.status !== 200) {
-      console.log(loginRes.body);
-      return;
-    }
-  })
+  // group("login", function () {
+  //   const loginRes = http.post(`${host}/auth/login`, payload, params)
+  //   check(loginRes, { 'response code for login was 200': (res) => res.status == 200 })
+  //   if (loginRes.status !== 200) {
+  //     console.log(loginRes.body);
+  //     return;
+  //   }
+  // })
 
 
-  const createURLPayload = JSON.stringify({
-    url: "https://www.bbc.com/",
-    monitoring: "none"
-  });
-  group("create url", function () {
-    const createRes = http.post("http://10.43.254.101:8080/create", createURLPayload, params)
-    check(createRes, { 'response code for create url was 201': (res) => res.status == 201 })
-    if (createRes.status !== 201) {
-      console.log(createRes.body)
-    }
-  })
+  // const createURLPayload = JSON.stringify({
+  //   url: "https://www.bbc.com/",
+  //   monitoring: "none"
+  // });
+  // group("create url", function () {
+  //   const createRes = http.post(`${host}/create`, createURLPayload, params)
+  //   check(createRes, { 'response code for create url was 201': (res) => res.status == 201 })
+  //   if (createRes.status !== 201) {
+  //     console.log(createRes.body)
+  //   }
+  // })
 
   // get the url
 
